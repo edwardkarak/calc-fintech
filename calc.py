7458fe2a23fd138ef66ce5eee789458810cb2ec6
Edward Karak <edkharakh@gmail.com>
2020-07-09 15:01:04 +0000

    first commit

---
+++ calc.py
@@ -0,0 +1,12 @@
+# Save to github:
+# 1. add the files u wanna save:
+# git add [filename]
+# git add .               add all files
+
+# 2. Name the saved version:
+#  git commit -m "name"
+
+# 3. Push the save to Github:
+# git push
+
+print("Calculator")
